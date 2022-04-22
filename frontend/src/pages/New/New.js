1951a99a09f5ff2e687b1cb5d6b75cf134e6f1ad
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-04-22 16:29:32 +0100

    Add new  Student/Teacher/Board Member element

--- frontend/src/pages/New/New.js
+++ frontend/src/pages/New/New.js
@@ -1,20 +1,42 @@
 import Card from '../../components/card/Card'
 import Navbar from '../../components/Navbar/Navbar'
 import './New.css'
-import {Contract} from 'ethers';
+import { Contract } from 'ethers';
+import { useState } from 'react';
+
 // import ABI from '../';
 
-export default function New() {
+export default function New()
+{
+  const [option, setOption] = useState('');
+  const handleOptionChange = (e) =>
+  {
+    setOption(e.target.value);
+  }
   return (
     <div className='new'>
       <Navbar />
       <div className='container'>
   
         <div className='container-body'>
-         {/* <Card title='Register New User' subtitle='Add new student to ZURI DAO'/> */}
-         <Card title='Add New Contestant' subtitle='Create New contestant to participate in election'/>
+         
+          
+          <Card title='Add New Contestant' subtitle='Create New contestant to participate in election' />
+          <div>
+            <select
+              name='option'
+              value={option}
+              onChange={handleOptionChange}
+            >
+             <option value="Student">Student</option> 
+             <option value="Teacher">Teacher</option> 
+             <option value="Board Member">Board Member</option> 
+          </select>
+          </div>
+          
         </div>
-      </div>
+        </div>
     </div>
   )
 }
+
